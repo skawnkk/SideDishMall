35be5fefa5c1004f25340721d82aa8af2d6c4c6a
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-04-28 18:44:35 +0900

    Feat: 라우팅 가능하게 만듬

--- frontend/src/LogIn.jsx
+++ frontend/src/LogIn.jsx
@@ -1,9 +1,15 @@
 
 import queryString from "query-string";
-
+import { useEffect } from 'react'
 const LogIn = (props) => {
     const result = queryString.parse(props.location.search);
     console.log(result)
+	console.log(props)
+	//localhost:8080/login?code={result.code} put 요청
+	useEffect(()=>{
+		props.history.push("/")
+	})
+	
     return (
 		<>
 		<h1>로그인 화면</h1>
