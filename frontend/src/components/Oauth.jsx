f2360dc0a4ab6ddf68769b933dcd414a4f82a80c
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-04-30 01:27:33 +0900

    Feat: localstorage islogin

--- frontend/src/components/Oauth.jsx
+++ frontend/src/components/Oauth.jsx
@@ -1,18 +1,26 @@
-import queryString from 'query-string';
-import { useEffect } from 'react';
-const Oauth = (props) => {
-	const result = queryString.parse(props.location.search);
-	console.log(result);
-	console.log(props);
-	//localhost:8080/login?code={result.code} put 요청
-	useEffect(() => {
-		props.history.push('/');
-	});
-
-	return (
-		<>
-			<h1>로그인 화면</h1>
-		</>
-	);
-};
-export default Oauth;
+import queryString from 'query-string'
+import styled from 'styled-components'
+import { useEffect } from 'react'
+const Oauth = props => {
+  const result = queryString.parse(props.location.search)
+  console.log(result)
+  console.log(props)
+  //localhost:8080/login?code={result.code} put 요청
+  //   useEffect(() => {
+  //     props.history.push('/')
+  //   })
+  const LogInLoading = styled.div`
+    position: fixed;
+    left: 0;
+    top: 0;
+    background: url('./login_load.gif') no-repeat center center;
+    width: 100%;
+    height: 100%;
+  `
+  return (
+    <>
+      <LogInLoading></LogInLoading>
+    </>
+  )
+}
+export default Oauth
